20f3a8b8de18513e6ddae7f35b9020a66a9b4b91
benuChang <benu@ucsb.edu>
2020-06-08 16:14:36 -0700

    Updates

---
+++ precache-manifest.2b1e8142120c27e129137f11774c0401.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6660875fc776e40b88604c95cd9e026",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c177386005de40992698",
+    "url": "./static/css/main.ddd8969c.chunk.css"
+  },
+  {
+    "revision": "711078761b1b01f48344",
+    "url": "./static/js/2.75cb29c6.chunk.js"
+  },
+  {
+    "revision": "0cfd7d68805081f2f24b32668e56f2b3",
+    "url": "./static/js/2.75cb29c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c177386005de40992698",
+    "url": "./static/js/main.adae92bd.chunk.js"
+  },
+  {
+    "revision": "69b9c42a23be6ac3c631",
+    "url": "./static/js/runtime-main.cc807781.js"
+  },
+  {
+    "revision": "ee65a27fb23cf0fba82ea3604e628b52",
+    "url": "./static/media/card.ee65a27f.jpg"
+  },
+  {
+    "revision": "93fe7c13982ac562b5bc9ca0793c695c",
+    "url": "./static/media/pic1.93fe7c13.jpg"
+  },
+  {
+    "revision": "074ea0452a8a956236934d5602e7366d",
+    "url": "./static/media/pic10.074ea045.jpg"
+  },
+  {
+    "revision": "14051480406f8b17a5678331ac55927e",
+    "url": "./static/media/pic11.14051480.jpg"
+  },
+  {
+    "revision": "35fe10c991c7f14130de52f3f2e4b211",
+    "url": "./static/media/pic12.35fe10c9.jpg"
+  },
+  {
+    "revision": "e39cb7c4e6559464e335755996f8fa98",
+    "url": "./static/media/pic13.e39cb7c4.jpg"
+  },
+  {
+    "revision": "6d28b8c72cd2d28235e7fb9ba49a3b1b",
+    "url": "./static/media/pic15.6d28b8c7.jpg"
+  },
+  {
+    "revision": "67d7d9c4246eae5ee8c306b7002064a0",
+    "url": "./static/media/pic16.67d7d9c4.jpg"
+  },
+  {
+    "revision": "8e2758fa2ad34c9dc7926b846da5bb41",
+    "url": "./static/media/pic2.8e2758fa.jpg"
+  },
+  {
+    "revision": "83c6d2ffacf2f1b5ccc483b9b1e4efaf",
+    "url": "./static/media/pic3.83c6d2ff.jpg"
+  },
+  {
+    "revision": "e1697275915e75942ca94ef578f1814c",
+    "url": "./static/media/pic4.e1697275.jpg"
+  },
+  {
+    "revision": "dc24212ba740cf407f3577fedeb260c2",
+    "url": "./static/media/pic5.dc24212b.jpg"
+  },
+  {
+    "revision": "f1d5115516895509dc9ec1e13feb38b0",
+    "url": "./static/media/pic6.f1d51155.jpg"
+  },
+  {
+    "revision": "c8e5a809700d9c0427d28e76e24e6098",
+    "url": "./static/media/pic7.c8e5a809.jpg"
+  },
+  {
+    "revision": "6b637ef8c818aebf3ed05bd86abdf01d",
+    "url": "./static/media/pic8.6b637ef8.jpg"
+  },
+  {
+    "revision": "d3973e8c92a382de44aa23ff5db5fc75",
+    "url": "./static/media/pic9.d3973e8c.jpg"
+  },
+  {
+    "revision": "fef23a6e1d7972a6302efd054a706999",
+    "url": "./static/media/sexinfo.fef23a6e.jpg"
+  },
+  {
+    "revision": "6b8cebfec5ae0ca13588a501fbc1a8e1",
+    "url": "./static/media/vid1.6b8cebfe.mp4"
+  },
+  {
+    "revision": "7d59bd10dadb66aa69388ee496f01837",
+    "url": "./static/media/vid2.7d59bd10.mp4"
+  },
+  {
+    "revision": "679ce782d2ccec05748ed66080c19195",
+    "url": "./static/media/vid3.679ce782.mp4"
+  },
+  {
+    "revision": "bf0abf5392abc273e063973a7872ffb2",
+    "url": "./static/media/vid4.bf0abf53.mp4"
+  },
+  {
+    "revision": "e580451810e29104a56649256b846748",
+    "url": "./static/media/vid5.e5804518.mp4"
+  },
+  {
+    "revision": "940da63ad49975ab16b61d8b6350a073",
+    "url": "./static/media/vid6.940da63a.mp4"
+  },
+  {
+    "revision": "314c0118900ce4c39fc64d29f8d11674",
+    "url": "./static/media/yan.314c0118.jpg"
+  }
+]);
